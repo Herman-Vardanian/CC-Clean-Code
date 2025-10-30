16d11269b2a0ecc01e9f8fac1dbec60f2200bdc8
Herman-Vardanian <vardanianherman@gmail.com>
2025-10-30 14:25:20 +0100

    dice between 1-6

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ export function analyzeDices(dices){
     if(dices.length != 5){
         throw new Error("Le nombre de dés doit être de 5")
     }
+
+    for(let i=0; i<dices.length; i++){
+        if(dices[i]< 1 || dices[i] > 6){
+            throw new Error("le dé n'est pas compris entre 1 et 6")
+        }
+    }
+
     return true;
 } 
--- index.test.js
+++ index.test.js
@@ -3,11 +3,18 @@ import { analyzeDices } from "/index.js";
 
 describe("Analyze dices to get points", () => {
     it("no errors if 5 dices", () => {
-    expect(analyzeDices([1, 2, 3, 4, 5])).toBe(true)
-  })
+        expect(analyzeDices([1, 2, 3, 4, 5])).toBe(true)
+    })
 
-  it("throw an error when there are not 5 dice", () => {
-    expect(() => analyzeDices([1, 2, 3, 4])).toThrow()
-    expect(() => analyzeDices([1, 2, 3, 4, 5, 6])).toThrow()
-  })
+    it("too many dices", () => {
+        expect(() => analyzeDices([1, 2, 3, 4, 5, 6])).toThrow()
+    })
+
+    it("not enough dices", () => {
+        expect(() => analyzeDices([1, 2, 3, 4])).toThrow()
+    })
+
+    it("one of dice number is not between 1-6", () => {
+        expect(() => analyzeDices([1, 2, 3, 4, 8])).toThrow()
+    })
 })
