0f039563a92bbedf8ad505d88970312f519d840b
Herman-Vardanian <vardanianherman@gmail.com>
2025-10-30 14:33:23 +0100

    input with rolls

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-export function analyzeDices(dices){
-    if(dices.length != 5){
-        throw new Error("Le nombre de dés doit être de 5")
-    }
+export function analyzeDiceRolls(allRolls){
 
-    for(let i=0; i<dices.length; i++){
-        if(dices[i]< 1 || dices[i] > 6){
-            throw new Error("le dé n'est pas compris entre 1 et 6")
+    for(let i = 0; i < allRolls.length; i++){
+        let diceRoll = allRolls[i]
+
+        if(diceRoll.length != 5){
+            throw new Error("Le nombre de dés doit être de 5")
+        }
+        for(let i=0; i<diceRoll.length; i++){
+            if(diceRoll[i]< 1 || diceRoll[i] > 6){
+                throw new Error("le dé n'est pas compris entre 1 et 6")
         }
     }
+    }
+    
+
+    
 
     return true;
 } 
--- index.test.js
+++ index.test.js
@@ -1,20 +1,20 @@
 import { describe, expect, it } from "vitest";
-import { analyzeDices } from "/index.js";
+import { analyzeDiceRolls } from "/index.js";
 
 describe("Analyze dices to get points", () => {
     it("no errors if 5 dices", () => {
-        expect(analyzeDices([1, 2, 3, 4, 5])).toBe(true)
+        expect(analyzeDiceRolls([[1, 2, 3, 4, 5], [5, 5, 5, 5, 5]])).toBe(true)
     })
 
     it("too many dices", () => {
-        expect(() => analyzeDices([1, 2, 3, 4, 5, 6])).toThrow()
+        expect(() => analyzeDiceRolls([[1, 2, 3, 4, 5, 6],[1, 2, 3, 4, 5]])).toThrow()
     })
 
     it("not enough dices", () => {
-        expect(() => analyzeDices([1, 2, 3, 4])).toThrow()
+        expect(() => analyzeDiceRolls([[1, 2, 3, 4],[1, 2, 3, 4, 5]])).toThrow()
     })
 
     it("one of dice number is not between 1-6", () => {
-        expect(() => analyzeDices([1, 2, 3, 4, 8])).toThrow()
+        expect(() => analyzeDiceRolls([[1, 2, 3, 4, 8],[1, 2, 3, 4, 5]])).toThrow()
     })
 })
